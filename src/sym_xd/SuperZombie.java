089e4e05b0a5b98f9e8a868cdcc9996df523b6fe
Barto9 <bartekgawryszuk@gmail.com>
2023-05-19 18:55:20 +0200

    Supery

---
+++ src/sym_xd/SuperZombie.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sym_xd;
+
+/**
+ *
+ * @author Bartek
+ */
+public class SuperZombie extends Zombie {
+    
+}
